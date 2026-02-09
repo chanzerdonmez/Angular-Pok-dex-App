31d4fbd838fb5bcb2abb9969ed0c9684c6d86335
Chanzer <chanzer@macbookpro.home>
2026-02-09 23:00:43 +0100

    displaying the state of a FormArray

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfileComponent } from './pokemon/pokemon-profile/pokemon-profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PokemonEditComponent } from './pokemon/pokemon-edit/pokemon-edit.component';
 
 const routes: Routes = [
-    { path: 'pokemons/:id', component: PokemonProfileComponent },
-  { path: 'pokemons', component: PokemonListComponent },
-  { path: '', redirectTo: '/pokemons',  pathMatch: 'full' }
+    { path: 'pokemons/edit/:id', component: PokemonEditComponent, title: "Édition d'un Pokémon" },
+
+  { path: 'pokemons/:id', component: PokemonProfileComponent, title: 'Pokémon' },
+  { path: 'pokemons', component: PokemonListComponent, title:'Pokédex' },
+  { path: '', redirectTo: '/pokemons',  pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+
 ];
 
 export const appConfig: ApplicationConfig = {
--- src/app/pokemon/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon/pokemon-list/pokemon-list.component.ts
@@ -3,11 +3,12 @@ import { PokemonService } from '../../pokemon.service';
 import { Pokemon } from '../../pokemon.model';
 import { DatePipe } from '@angular/common';
 import { PokemonBorderDirective } from '../../pokemon-border.directive';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [PokemonBorderDirective, DatePipe],
+  imports: [PokemonBorderDirective, DatePipe, RouterLink],
   templateUrl: './pokemon-list.component.html',
   styles: ``
 })
